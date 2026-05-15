5b17c4e5f6c10b9e2134c15d66429ac997209e56
marcusleonas <wishbone@posteo.uk>
2026-05-15 12:03:30 +0100

    add banner for backup sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), svelte()],
   build: {
     format: "preserve",
   },
---
+++ src/components/backup-banner.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  const currentHost = window.location.hostname;
+  const isBackupDomain = currentHost !== "n3bula.lol";
+</script>
+
+{#if isBackupDomain}
+  <div class="backup-banner">
+    <p>
+      You are currently visiting a backup version of my site, so some things may
+      not work. The main is hosted at <a href="https://n3bula.lol">n3bula.lol</a
+      > :3
+    </p>
+  </div>
+{/if}
+
+<style>
+  .backup-banner {
+    position: relative;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    background: #d46408;
+    text-align: center;
+    padding: 5px 0;
+  }
+</style>
